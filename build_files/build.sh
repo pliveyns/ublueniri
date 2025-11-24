9f524a086b395b622b22f5751d420f0f474d6d3b
Peter Liveyns <peter.liveyns@gmail.com>
2025-11-24 21:32:44 +0100

    debug build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Enable use of copr repos
-#dnf install -y 'dnf5-command(config-manager)'
+dnf install -y 'dnf5-command(config-manager)'
 
 # this installs a package from fedora repos
 #dnf5 install -y tmux
