27b5b085aeddbcd9a1948bf21f2c4de0b082b818
Peter Liveyns <peter.liveyns@gmail.com>
2025-11-24 19:56:45 +0100

    debug build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -32,8 +32,8 @@ dnf5 install -y langpacks-en_GB \
 # dnf5 -y copr disable ublue-os/staging
 dnf5 -y copr enable ublue-os/packages
 dnf5 -y install uupd \
-  ublue-setup-service \
-  ublue-os-udeb-rules \
+  ublue-setup-services \
+  ublue-os-udev-rules \
   ublue-os-signing \
   ublue-os-luks \
   ublue-os-just
