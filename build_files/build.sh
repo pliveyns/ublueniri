3b127f72a01a54909d0eae6a88d15331a7014330
Peter Liveyns <peter.liveyns@gmail.com>
2025-11-24 19:49:59 +0100

    debug build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,6 +9,9 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Enable use of copr repos
+dnf install -y 'dnf5-command(config-manager)'
+
 # this installs a package from fedora repos
 #dnf5 install -y tmux
 dnf5 install -y langpacks-en_GB \
