f211c0886305c716f54948a06167b3302525d2bb
Peter Liveyns <peter.liveyns@gmail.com>
2025-11-25 20:59:04 +0100

    Add niri and dms

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,7 +14,7 @@ dnf install -y 'dnf5-command(config-manager)'
 
 # this installs a package from fedora repos
 #dnf5 install -y tmux
-dnf5 install -y langpacks-en_GB \
+dnf install -y langpacks-en_GB \
   distrobox \
   alacritty \
   kitty \
@@ -30,14 +30,31 @@ dnf5 install -y langpacks-en_GB \
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
-dnf5 -y copr enable ublue-os/packages
-dnf5 -y install uupd \
+dnf copr enable -y ublue-os/packages
+dnf install -y uupd \
   ublue-setup-services \
   ublue-os-udev-rules \
   ublue-os-luks \
   ublue-os-just
-dnf5 -y copr disable ublue-os/packages
+dnf copr disable -y ublue-os/packages
+
+dnf copr enable -y yalter/niri-git
+dnf copr enable -y avengemedia/dms-git
+dnf copr enable -y avengemedia/danklinux
+dnf install -y libdisplay-info \
+  libseat \
+  xwayland-satellite \
+  niri \
+  dms \
+  dms-cli \
+  dms-greeter \
+  dgop
+dnf copr disable -y yalter/niri-git
+dnf copr disable -y avengemedia/dms-git
+dnf copr disable -y avengemedia/danklinux
 
 #### Example for enabling a System Unit File
 
 #systemctl enable podman.socket
+systemctl enable greetd.service
+systemctl enable --global dms.service
